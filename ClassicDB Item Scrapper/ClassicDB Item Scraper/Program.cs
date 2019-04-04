dfb71d87b99c4aa46048327d052ffdb002ff9a14
Nathaniel Arms <nathaniel.arms@gmail.com>
2019-04-04 17:19:48 -0500

    .

--- ClassicDB Item Scrapper/ClassicDB Item Scraper/Program.cs
+++ ClassicDB Item Scrapper/ClassicDB Item Scraper/Program.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
+using System.Net.Http;
+using System.Xml.XPath;
 using System.Text.RegularExpressions;
+using HtmlAgilityPack;
 
 namespace ClassicDB_Item_Scraper
 {
@@ -13,22 +16,41 @@ namespace ClassicDB_Item_Scraper
     {
         static void Main(string[] args)
         {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+            
+            /*
             WebClient wc = new WebClient();
 
             string raw = wc.DownloadString("https://classicdb.ch/?item=16802");
-
-            //System.Web.e
-
+                        
             Regex iLvl = new Regex("(?=<div>Level: )([^\n\r]*)");
             Match iLvlR = iLvl.Match(raw);
 
             string iLvlResult = iLvlR.ToString().Replace(@"<div>Level: ", "").Replace(@"</div>","");
-            //string iLvlReplaced = iLvlR[0].replace(@"<div>Level: ", "");
+            
             Console.WriteLine(iLvlResult);
-            //string webData = Encoding.UTF8.GetString(raw);
+            
 
             string reader = File.ReadAllText(raw, Encoding.UTF8);
-            //StreamWriter writer = File.WriteAllText(@"C:\GitHub\natearms\WoW\ClassicDB Item Scrapper\ClassicDB Item Scraper\Files\ClassicDb-Parse.txt", reader);
+            */
+
+
+
+        }
+        async static Task MainAsync(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://classicdb.ch/?item=16802");
+            var pageContents = await response.Content.ReadAsStringAsync();
+
+            HtmlDocument pageDocument = new HtmlDocument();
+            pageDocument.LoadHtml(pageContents);
+
+            IEnumerable<string> listItemHtml = pageDocument.DocumentNode
+                .SelectNodes("//div[contains(@class, 'infobox-spacer')]").Select(li => li.OuterHtml);
+}
+            Console.WriteLine("1");
+            Console.ReadLine();
         }
     }
 }
