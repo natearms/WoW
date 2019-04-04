6b2d3a1e9561bd02464c5456cca1e93ce77b4458
Nathaniel Arms <nathaniel.arms@gmail.com>
2019-04-04 08:16:14 -0500

    .

---
+++ ClassicDB Item Scrapper/ClassicDB Item Scraper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ClassicDB_Item_Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient wc = new WebClient();
+
+            string raw = wc.DownloadString("https://classicdb.ch/?item=16802");
+
+            //System.Web.e
+
+            Regex iLvl = new Regex("(?=<div>Level: )([^\n\r]*)");
+            Match iLvlR = iLvl.Match(raw);
+
+            string iLvlResult = iLvlR.ToString().Replace(@"<div>Level: ", "").Replace(@"</div>","");
+            //string iLvlReplaced = iLvlR[0].replace(@"<div>Level: ", "");
+            Console.WriteLine(iLvlResult);
+            //string webData = Encoding.UTF8.GetString(raw);
+
+            string reader = File.ReadAllText(raw, Encoding.UTF8);
+            //StreamWriter writer = File.WriteAllText(@"C:\GitHub\natearms\WoW\ClassicDB Item Scrapper\ClassicDB Item Scraper\Files\ClassicDb-Parse.txt", reader);
+        }
+    }
+}
