f49f710786c52b4fbd562fac3f434f5e7988ace4
Nathaniel Arms <nathaniel.arms@gmail.com>
2019-06-12 17:56:37 -0500

    .

--- The House Bot/The House Bot/Program.cs
+++ The House Bot/The House Bot/Program.cs
@@ -59,9 +59,23 @@ namespace The_House_Bot
             await Client.SetGameAsync("The House Bot", "https://google.com", ActivityType.Streaming);
         }
 
-        private async Task Client_MessageReceived(SocketMessage arg)
+        private async Task Client_MessageReceived(SocketMessage MessageParam)
         {
-            //configure the commands
+            var Message = MessageParam as SocketUserMessage;
+            var Context = new SocketCommandContext(Client, Message);
+
+            if (Context.Message == null || Context.Message.Content == "") return;
+            if (Context.User.IsBot) return;
+
+            int ArgPos = 0;
+
+            if (!(Message.HasStringPrefix("a!", ref ArgPos) || Message.HasMentionPrefix(Client.CurrentUser, ref ArgPos))) return;
+
+            var Result = await Commands.ExecuteAsync(Context, ArgPos, services: null);
+            if (!Result.IsSuccess)
+            {
+                Console.WriteLine($"{DateTime.Now} at Commands] Something went wrong with executing a command. Text: {Context.Message.Content} | Error: {Result.ErrorReason}");
+            }
         }
 
     }
