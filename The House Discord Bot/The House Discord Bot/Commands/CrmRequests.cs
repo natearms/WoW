19f734e25c180d88ee57ab2a28bc01720ce486c5
Nathaniel Arms <nathaniel.arms@gmail.com>
2019-06-20 23:12:14 -0500

    Bot Command updates

--- The House Discord Bot/The House Discord Bot/Commands/CrmRequests.cs
+++ The House Discord Bot/The House Discord Bot/Commands/CrmRequests.cs
@@ -26,16 +26,24 @@ namespace The_House_Discord_Bot.Commands
         public class DkpModule : ModuleBase<SocketCommandContext>
         {
             public IOrganizationService crmService { get; set; }
+            
             [Command("")]
             public async Task PrEpGp()
             {
-                await ReplyAsync(null, false, BuildUsersDKP(Context.Message.Author));
-            }
+                List<SocketUser> list = new List<SocketUser>();
+                list.Add(Context.Message.Author);
+                list.AsReadOnly();
 
+                IReadOnlyCollection<SocketUser> queriedUser = list;
+                await ReplyAsync(null, false, BuildUsersDKP(queriedUser).Build());
+            }
+            
+            
             [Command("")]
             public async Task PrEpGpMention(IUser mentionedUser)
             {
-                await ReplyAsync(null, false, BuildUsersDKP(mentionedUser));
+                IReadOnlyCollection<SocketUser> mentionedUsers = Context.Message.MentionedUsers;
+                await ReplyAsync(null, false, BuildUsersDKP(mentionedUsers).Build());
             }
 
             [Command("")]
@@ -45,19 +53,38 @@ namespace The_House_Discord_Bot.Commands
 
                 if (mentionedUsers.Count == 0)
                     await ReplyAsync("You must provide a mention in order to use the multi mention feature");
-                else if (mentionedUsers.Count > 10)
+                else if (mentionedUsers.Count>10)
                 {
                     await ReplyAsync("You've requested too many users, please limit results to 10 at a time.");
                 }
                 else
                 {
-                    for (int i = 0; i < mentionedUsers.Count; i++)
-                    {
-                        await ReplyAsync(null, false, BuildUsersDKP(mentionedUsers.ElementAt(i)));
-                        System.Threading.Thread.Sleep(1000);
-                    }
+                    await ReplyAsync(null, false, BuildUsersDKP(mentionedUsers).Build());
                 }
+            }
+            /*
+            [Command("-top")]
+            public async Task PrEpGpMentions(int returnRange)
+            {
+                EntityCollection topUsers = GetTopUserEpGp(returnRange, crmService);
+                
 
+                await ReplyAsync(null, false, BuildUsersDKP(mentionedUsers).Build());
+                
+            }
+            */
+            private EntityCollection GetTopUserEpGp(int returnCount, IOrganizationService crmService)
+            {
+                QueryExpression query = new QueryExpression("contact");
+                query.ColumnSet.AddColumns("wowc_totalpr", "wowc_totalep", "wowc_totalgp");
+                query.Criteria = new FilterExpression();
+                //query.Criteria.AddCondition("lastname", ConditionOperator.Equal, userName);
+                query.Orders.Add(new OrderExpression("wowc_totalpr", OrderType.Descending));
+                query.PageInfo.Count = returnCount;
+
+                EntityCollection results = crmService.RetrieveMultiple(query);
+
+                return results;
             }
             private EntityCollection GetUserEpGp(string userName, IOrganizationService crmService)
             {
@@ -65,37 +92,47 @@ namespace The_House_Discord_Bot.Commands
                 query.ColumnSet.AddColumns("wowc_totalpr", "wowc_totalep", "wowc_totalgp");
                 query.Criteria = new FilterExpression();
                 query.Criteria.AddCondition("lastname", ConditionOperator.Equal, userName);
+                query.Orders.Add(new OrderExpression("wowc_totalpr", OrderType.Descending));
 
                 EntityCollection results = crmService.RetrieveMultiple(query);
 
                 return results;
             }
-            private Embed BuildUsersDKP(IUser providedUser)
+            private EmbedBuilder BuildUsersDKP(IReadOnlyCollection<SocketUser> providedUser)
             {
-                string guildNickname = Context.Guild.GetUser(providedUser.Id).Nickname;
-                string userNickname = providedUser.Username;
-
-                string userName = guildNickname == null ? userNickname : guildNickname;
-                var userNameMention = providedUser.Mention;
-
-                EntityCollection userInfo = GetUserEpGp(userName, crmService);
+                
                 EmbedBuilder prBuilder = new EmbedBuilder();
-                if (userInfo.Entities.Count == 0)
+                
+                string commentString = "```" + "Name".PadRight(15) + "Total PR".PadLeft(12) + "Total EP".PadLeft(12) + "Total GP".PadLeft(12);
+                string mentionGuildOwner = "";
+                for (int i = 0; i < providedUser.Count; i++)
                 {
-                    prBuilder.WithDescription("I could not find a DKP record for " + userNameMention + " in <The House> CRM.  Please contact " +
-                        Context.Guild.Owner.Mention + " to create a record for this user.");
-                }
-                else
-                {
-                    prBuilder.WithDescription("**Here is the DKP breakdown for** " + userNameMention +
-                    "\n\n**PR: **" + userInfo.Entities[0].GetAttributeValue<Decimal>("wowc_totalpr").ToString("0.##") +
-                    "\n**EP: **" + userInfo.Entities[0].GetAttributeValue<Decimal>("wowc_totalep").ToString("0.##") +
-                    "\n**GP: **" + userInfo.Entities[0].GetAttributeValue<Decimal>("wowc_totalgp").ToString("0.##") + "\n"
-                    )
-                    ;
-                }
+                    
+                    string guildNickname = Context.Guild.GetUser(providedUser.ElementAt(i).Id).Nickname;
+                    string userNickname = providedUser.ElementAt(i).Username;
 
-                return prBuilder.Build();
+                    string userName = guildNickname == null ? userNickname : guildNickname;
+                    EntityCollection userInfo = GetUserEpGp(userName, crmService);
+
+                    if (userInfo.Entities.Count == 0)
+                    {
+                        commentString += ("\n"+ userName.PadRight(15,'.') + "doesn't seem to be in CRM.....".PadLeft(36,'.'));
+                        mentionGuildOwner = Context.Guild.Owner.Mention +", one or more users are not in CRM."; 
+                    }
+                    else
+                    {
+                        string totalPr = userInfo.Entities[0].GetAttributeValue<Decimal>("wowc_totalpr").ToString("0.##");
+                        string totalEp = userInfo.Entities[0].GetAttributeValue<Decimal>("wowc_totalep").ToString("0.##");
+                        string TotalGp = userInfo.Entities[0].GetAttributeValue<Decimal>("wowc_totalgp").ToString("0.##");
+
+                        commentString += "\n" + userName.PadRight(15, '.') + totalPr.ToString().PadLeft(12, '.') + totalEp.ToString().PadLeft(12, '.') + TotalGp.ToString().PadLeft(12, '.');
+
+                    }
+                }
+                commentString += "```";
+                prBuilder.WithDescription(mentionGuildOwner +"\n"+ commentString)
+                ;
+                return prBuilder;
             }
         }
         
@@ -208,7 +245,7 @@ namespace The_House_Discord_Bot.Commands
                 string userNickname = author.Username;
                 string userName = guildNickname == null ? userNickname : guildNickname;
 
-                await ReplyAsync(AssociateRecords(crmService, GetUserInformation(userName, crmService), GetItemInformation(itemSearch, crmService)), false, null);
+                await ReplyAsync(AssociateRecords(crmService, GetUserInformation(userName, crmService), GetItemInformation(itemSearch, crmService), userName,Context.Guild.Owner), false, null);
             }
 
             [Command("-remove"), Summary("Removes a recipe to the current user.")]
@@ -219,13 +256,22 @@ namespace The_House_Discord_Bot.Commands
                 string userNickname = author.Username;
                 string userName = guildNickname == null ? userNickname : guildNickname;
 
-                await ReplyAsync(DisassociateRecords(crmService, GetUserInformation(userName, crmService), GetItemInformation(itemSearch, crmService)), false, null);
+                await ReplyAsync(DisassociateRecords(crmService, GetUserInformation(userName, crmService), GetItemInformation(itemSearch, crmService), userName, Context.Guild.Owner), false, null);
             }
             [Command("-search"), Summary("Searches users that know this recipe.")]
             public async Task searchRecipe([Remainder] string itemSearch)
             {
                 //RetrieveUsersWithRecipe(crmService, itemSearch);
-                await ReplyAsync("results", false, RecipeSearchEmbedBuilder(crmService,itemSearch).Build());
+                await ReplyAsync(null, false, RecipeSearchEmbedBuilder(crmService,itemSearch, Context.Guild.Owner).Build());
+            }
+            [Command("-search"), Summary("Returns recipes that this user knows.")]
+            public async Task searchRecipe(IUser providedUser)
+            {
+                string guildNickname = Context.Guild.GetUser(providedUser.Id).Nickname;
+                string userNickname = providedUser.Username;
+                string userName = guildNickname == null ? userNickname : guildNickname;
+
+                await ReplyAsync(null, false, UserRecipeResults(crmService, GetUserInformation(userName,crmService), userName, Context.Guild.Owner).Build());
             }
             private static EntityCollection GetItemInformation(string itemSearch, IOrganizationService crmService)
             {
@@ -244,20 +290,20 @@ namespace The_House_Discord_Bot.Commands
                 QueryExpression query = new QueryExpression("contact");
                 query.ColumnSet.AddColumns("lastname", "wowc_primaryprofession", "wowc_secondaryprofession");
                 query.Criteria = new FilterExpression();
-                query.Criteria.AddCondition("lastname", ConditionOperator.Like, "%" + userName + "%");
+                query.Criteria.AddCondition("lastname", ConditionOperator.Equal, userName);
                 query.Orders.Add(new OrderExpression("lastname", OrderType.Ascending));
 
                 EntityCollection results = crmService.RetrieveMultiple(query);
                 return results;
             }
 
-            private static string AssociateRecords(IOrganizationService crmService, EntityCollection contact, EntityCollection wowc_loot)
+            private static string AssociateRecords(IOrganizationService crmService, EntityCollection contact, EntityCollection wowc_loot, string mentionedUser, IUser guildOwner)
             {
                 string results = "";
                 if (contact.Entities.Count < 1)
-                    return results = "Sorry, I could not find you in CRM!";
+                    return results = "Sorry, I could not find you in CRM, please make sure that your server nickname matches your WoW character name and try again. " + guildOwner.Mention + ", can you make sure " + mentionedUser + " exists?";
                 else if (contact.Entities.Count > 1)
-                    return results = "There seems to be more than one of you in CRM...";
+                    return results = "There seems to be more than one of you in CRM... " + guildOwner.Mention + ", could you look into this?";
                 else if (wowc_loot.Entities.Count < 1)
                     return results = "I could not find the item you are searching for.";
                 else if (wowc_loot.Entities.Count > 1)
@@ -277,13 +323,13 @@ namespace The_House_Discord_Bot.Commands
                     return results = "Hmm, this shouldn't have happened...";   
             }
 
-            private static string DisassociateRecords(IOrganizationService crmService, EntityCollection contact, EntityCollection wowc_loot)
+            private static string DisassociateRecords(IOrganizationService crmService, EntityCollection contact, EntityCollection wowc_loot, string mentionedUser, IUser guildOwner)
             {
                 string results = "";
                 if (contact.Entities.Count < 1)
-                    return results = "Sorry, I could not find you in CRM!";
+                    return results = "Sorry, I could not find you in CRM, please make sure that your server nickname matches your WoW character name and try again. " +guildOwner.Mention+", can you make sure "+mentionedUser+" exists?";
                 else if (contact.Entities.Count > 1)
-                    return results = "There seems to be more than one of you in CRM...";
+                    return results = "There seems to be more than one of you in CRM... " + guildOwner.Mention +" could you look into this?";
                 else if (wowc_loot.Entities.Count < 1)
                     return results = "I could not find the item you are searching for.";
                 else if (wowc_loot.Entities.Count > 1)
@@ -304,8 +350,10 @@ namespace The_House_Discord_Bot.Commands
                     return results = "Hmm, this shouldn't have happened...";
             }
 
-            private EmbedBuilder RecipeSearchEmbedBuilder(IOrganizationService crmService, string itemSearch)
+            private EmbedBuilder RecipeSearchEmbedBuilder(IOrganizationService crmService, string itemSearch, IUser guildOwner)
             {
+                EmbedBuilder prBuilder = new EmbedBuilder();
+
                 string entity1 = "wowc_loot";
                 string entity2 = "contact";
                 string relationshipEntityName = "wowc_contact_wowc_loot";
@@ -325,26 +373,74 @@ namespace The_House_Discord_Bot.Commands
                 
                 EntityCollection results = crmService.RetrieveMultiple(query);
 
-                EmbedBuilder prBuilder = new EmbedBuilder();
-                var string1 = results.Entities[0].Attributes.Keys.ToArray();
-
-                string commentString = "```" + "Guild Member".PadRight(13) + "Item Name".PadLeft(40);
-                for (int i = 0; i < results.Entities.Count; i++)
+                if (results.Entities.Count < 1)
+                    return prBuilder.WithDescription("It seems like no one has what you are looking for, or you mispelled what you were looking for.");
+                else
                 {
-                    var playerName = results.Entities[i].GetAttributeValue<AliasedValue>("contact2.lastname").Value.ToString();
-                    string recipe = results.Entities[i].GetAttributeValue<string>("wowc_name").ToString();
-                    recipe = recipe.Substring(recipe.LastIndexOf(':') + 1).TrimStart(' ');
-                    
-                    playerName = playerName.Length > 12 ? playerName.Substring(0, 12) : playerName;
-                    recipe = recipe.Length > 40 ? recipe.Substring(0, 40) : recipe;
+                    string commentString = "```" + "Guild Member".PadRight(13) + "Item Name".PadLeft(40);
+                    for (int i = 0; i < results.Entities.Count; i++)
+                    {
+                        var playerName = results.Entities[i].GetAttributeValue<AliasedValue>("contact2.lastname").Value.ToString();
+                        string recipe = results.Entities[i].GetAttributeValue<string>("wowc_name").ToString();
+                        recipe = recipe.Substring(recipe.LastIndexOf(':') + 1).TrimStart(' ');
 
-                    commentString += "\n" + playerName.PadRight(13, '.') + recipe.ToString().PadLeft(40,'.');
+                        playerName = playerName.Length > 12 ? playerName.Substring(0, 12) : playerName;
+                        recipe = recipe.Length > 40 ? recipe.Substring(0, 40) : recipe;
 
+                        commentString += "\n" + playerName.PadRight(13, '.') + recipe.ToString().PadLeft(40, '.');
+
+                    }
+                    commentString += "```";
+                    prBuilder.WithDescription(commentString)
+                        ;
                 }
-                commentString += "```";
-                prBuilder.WithDescription(commentString)
-                    ;
 
+                return prBuilder;
+            }
+            private static EmbedBuilder UserRecipeResults(IOrganizationService crmService, EntityCollection contact, string mentionedUser, IUser guildOwner)
+            {
+                EmbedBuilder prBuilder = new EmbedBuilder();
+
+                if (contact.Entities.Count < 1)
+                    return prBuilder.WithDescription("I could not find "+mentionedUser+" in CRM... Hey" + guildOwner.Mention+" could you make sure "+mentionedUser+" exists in CRM" );
+                else if(contact.Entities.Count > 1)
+                    return prBuilder.WithDescription(guildOwner.Mention+", there seems to be more than 1 " + contact.Entities[0].GetAttributeValue<string>("lastname")+" in CRM...");
+
+                string entity1 = "wowc_loot";
+                string entity2 = "contact";
+                string relationshipEntityName = "wowc_contact_wowc_loot";
+
+                QueryExpression query = new QueryExpression(entity1);
+                query.ColumnSet = new ColumnSet("wowc_name", "wowc_lootid", "wowc_slot");
+
+                LinkEntity linkEntity1 = new LinkEntity(entity1, relationshipEntityName, "wowc_lootid", "wowc_lootid", JoinOperator.Inner);
+                LinkEntity linkEntity2 = new LinkEntity(relationshipEntityName, entity2, "contactid", "contactid", JoinOperator.Inner);
+                linkEntity2.Columns.AddColumns("lastname");
+                linkEntity2.LinkCriteria.AddCondition("lastname", ConditionOperator.Like, "%" + contact.Entities[0].GetAttributeValue<string>("lastname") + "%");
+                linkEntity1.LinkEntities.Add(linkEntity2);
+                query.LinkEntities.Add(linkEntity1);
+                //query.Criteria.AddCondition("wowc_name", ConditionOperator.Like, "%" + itemSearch + "%");
+                //query.Criteria.AddCondition("wowc_slot", ConditionOperator.Equal, 257260010);
+                query.Orders.Add(new OrderExpression("wowc_name", OrderType.Ascending));
+
+                EntityCollection results = crmService.RetrieveMultiple(query);
+
+                if (results.Entities.Count < 1)
+                    prBuilder.WithDescription("It doesn't look like " + contact.Entities[0].GetAttributeValue<string>("lastname") + " has any known recipes.");
+                else if(results.Entities.Count >= 1)
+                {
+                    string commentString = "```";
+                    for (int i = 0; i < results.Entities.Count; i++)
+                    {
+                        string recipe = results.Entities[i].GetAttributeValue<string>("wowc_name").ToString();
+                        //recipe = recipe.Substring(recipe.LastIndexOf(':') + 1).TrimStart(' ');
+                        commentString += "\n" + recipe.ToString();
+
+                    }
+                    commentString += "```";
+                    prBuilder.WithDescription(commentString)
+                        .WithTitle(contact.Entities[0].GetAttributeValue<string>("lastname") + " has listed the following recipes as known.");
+                }
                 return prBuilder;
             }
         }
