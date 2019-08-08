a178b251c02e4f37d798310043f10457411fa279
Nathaniel Arms <nathaniel.arms@gmail.com>
2019-08-08 17:59:34 -0500

    .

--- The House Discord Bot/The House Discord Bot/TheHouseBot.cs
+++ The House Discord Bot/The House Discord Bot/TheHouseBot.cs
@@ -9,12 +9,14 @@ using System.Reflection;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Web.Services.Description;
+using System.Net.Http;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Tooling.Connector;
+using Newtonsoft.Json.Linq;
 using Microsoft.Xrm.Sdk.Query;
 using ServiceCollection = Microsoft.Extensions.DependencyInjection.ServiceCollection;
 
@@ -28,6 +30,8 @@ namespace The_House_Discord_Bot
         private IServiceProvider _servicePriveProvider;
         private IOrganizationService _crmConn;
         private string botTrigger = "thb! ";
+        private int recurrenceFlag = 0;
+        private int barrensChatActivity = 0;
 
         static void Main(string[] args)
             => new TheHouseBot().MainAsync().GetAwaiter().GetResult();
@@ -67,6 +71,22 @@ namespace The_House_Discord_Bot
         private async Task Client_Log(LogMessage Message)
         {
             Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+            if (Message.Message == "Received HeartbeatAck")
+            {
+                recurrenceFlag++;
+                
+             if(recurrenceFlag > 15 && barrensChatActivity > 5)
+                {
+                    HttpClient client = new HttpClient();
+                    string returnString = await client.GetStringAsync("http://api.icndb.com/jokes/random");
+                    JObject o = JObject.Parse(returnString);
+                    string joke = (string)o["value"]["joke"];
+
+                    await ((ISocketMessageChannel)_client.GetChannel(584775200642564141)).SendMessageAsync(joke.ToString());
+                    recurrenceFlag = 0;
+                    barrensChatActivity = 0;
+                }
+            }
         }
 
         private async Task Client_Ready()
@@ -79,18 +99,24 @@ namespace The_House_Discord_Bot
             var Message = MessageParam as SocketUserMessage;
             var Context = new SocketCommandContext(_client, Message);
             int ArgPos = 0;
+            
+            if(Message.Channel.Id == 584775200642564141)
+            {
+                barrensChatActivity++;
+            }
 
             if (Context.User.IsBot) return;
             if (Context.Message == null || Context.Message.Content == "") return;
             if (!(Message.HasStringPrefix(botTrigger, ref ArgPos) || Message.HasMentionPrefix(_client.CurrentUser, ref ArgPos))) return;
 
             var Result = await _command.ExecuteAsync(Context, ArgPos, _servicePriveProvider);
-            
+                        
             if (!Result.IsSuccess)
             {
                 Console.WriteLine($"{DateTime.Now} at Commands] Something went wrong with executing a command. Text: {Context.Message.Content} | Error: {Result.ErrorReason}");
                 await Context.Channel.SendMessageAsync("Sorry but I did not recognize this command, please type **" + botTrigger + "-help** for a list of available commands that I understand.", false, null);
             }
         }
+        
     }
 }
