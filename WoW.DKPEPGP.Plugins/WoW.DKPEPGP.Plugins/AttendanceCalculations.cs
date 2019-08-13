e1eed98bca3c376aaef81dc4f67f2e8d8dfc9a99
Nathaniel Arms <nathaniel.arms@gmail.com>
2019-08-13 17:33:44 -0500

    Attendance calculations

--- WoW.DKPEPGP.Plugins/WoW.DKPEPGP.Plugins/AttendanceCalculations.cs
+++ WoW.DKPEPGP.Plugins/WoW.DKPEPGP.Plugins/AttendanceCalculations.cs
@@ -23,7 +23,55 @@ namespace WoW.DKPEPGP.Plugins
 
             try
             {
+                EntityCollection raidMembers = GetRaidMembers(service);
 
+                foreach (var members in raidMembers.Entities)
+                {
+                    DateTime trialEnd = members.GetAttributeValue<DateTime>("wowc_trialend");
+                    tracingService.Trace("Trial End " + trialEnd);
+
+                    Guid memberGuid = members.GetAttributeValue<Guid>("contactid");
+                    tracingService.Trace("Member Guid " + memberGuid);
+
+                    int lastThirtyAttendance = MemberAttendance(DateTime.Now.AddDays(-30), memberGuid, service);
+                    int lastThirtyRaids = AttendanceRecords(DateTime.Now.AddDays(-30), service);
+
+                    int lastSixtyAttendance = MemberAttendance(DateTime.Now.AddDays(-60), memberGuid, service);
+                    int lastSixtyRaids = AttendanceRecords(DateTime.Now.AddDays(-60), service);
+
+                    int totalMemberAttendance = MemberAttendance(trialEnd, memberGuid, service);
+                    int totalMemberTrialEndRaids = AttendanceRecords(trialEnd, service);
+
+                    tracingService.Trace("Counts: Member 30 = {0} - Raid 30 = {1} - Member 60 = {2} - Raid 60 = {3} - Total Member Attendance = {4} - Total Raids From Trail = {5} ", lastThirtyAttendance, lastThirtyRaids, lastSixtyAttendance, lastSixtyRaids, totalMemberAttendance, totalMemberTrialEndRaids);
+
+                    Double thirtyDays = 0;
+                    Double sixtyDays = 0;
+                    Double totalAttendance = 0;
+
+                    if (lastThirtyAttendance > 0)
+                    {
+                        thirtyDays = ((Double)lastThirtyAttendance / (Double)lastThirtyRaids)*100;
+                    }
+                    if (lastSixtyAttendance > 0)
+                    {
+                        sixtyDays = ((Double)lastSixtyAttendance / (Double)lastSixtyRaids)*100;
+                    }
+                    if (totalMemberAttendance > 0)
+                    {
+                        totalAttendance = ((Double)totalMemberAttendance / (Double)totalMemberTrialEndRaids)*100;
+                    }
+
+                    tracingService.Trace("Thirty% = {0} : Sixty% = {1} : Total% - {2}", thirtyDays, sixtyDays, totalAttendance);
+
+                    Entity entity = new Entity("contact");
+
+                    entity.Id = memberGuid;
+                    entity["wowc_30daysattendance"] = (Double)thirtyDays;
+                    entity["wowc_60daysattendance"] = (Double)sixtyDays;
+                    entity["wowc_attendanceoverall"] = (Double)totalAttendance;
+                    service.Update(entity);
+
+                }
             }
             catch (FaultException<OrganizationServiceFault> e)
             {
@@ -31,5 +79,46 @@ namespace WoW.DKPEPGP.Plugins
                 throw;
             }
         }
+        private static EntityCollection GetRaidMembers(IOrganizationService service)
+        {
+            QueryExpression query = new QueryExpression("contact");
+            query.ColumnSet.AddColumns("contactid", "wowc_trialend");
+            query.Criteria = new FilterExpression();
+            query.Criteria.AddCondition("statecode", ConditionOperator.Equal, "Active");
+
+            EntityCollection results = service.RetrieveMultiple(query);
+            return results;
+        }
+        
+        private static int AttendanceRecords(DateTime trialEndDate, IOrganizationService service)
+        {
+            QueryExpression query = new QueryExpression("wowc_attendance");
+            query.ColumnSet.AddColumns("wowc_attendanceid");
+            query.Criteria = new FilterExpression();
+            query.Criteria.AddCondition("wowc_attendancedate", ConditionOperator.GreaterEqual, trialEndDate);
+
+            int results = service.RetrieveMultiple(query).Entities.Count;
+            return results;
+        }
+        private static int MemberAttendance(DateTime trialEndDate, Guid contactGuid, IOrganizationService service)
+        {
+            string entity1 = "wowc_attendance";
+            string entity2 = "contact";
+            string relationshipEntityName = "wowc_wowc_attendance_contact";
+            QueryExpression query = new QueryExpression(entity1);
+            //query.ColumnSet = new ColumnSet("wowc_attendanceid");
+
+            LinkEntity linkEntity1 = new LinkEntity(entity1, relationshipEntityName, "wowc_attendanceid", "wowc_attendanceid", JoinOperator.Inner);
+            LinkEntity linkEntity2 = new LinkEntity(relationshipEntityName, entity2, "contactid", "contactid", JoinOperator.Inner);
+            linkEntity2.LinkCriteria.AddCondition("contactid", ConditionOperator.Equal, contactGuid);
+            linkEntity1.LinkEntities.Add(linkEntity2);
+            query.LinkEntities.Add(linkEntity1);
+            query.Criteria.AddCondition("wowc_attendancedate", ConditionOperator.GreaterEqual, trialEndDate);
+
+            int results = service.RetrieveMultiple(query).Entities.Count;
+
+            return results;
+        }
+
     }
 }
