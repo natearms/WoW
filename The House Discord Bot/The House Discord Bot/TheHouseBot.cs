cb98f40c9ebe0e0dec41f617a2ecf368fe15256d
Nathaniel Arms <nathaniel.arms@gmail.com>
2019-08-13 15:54:26 -0500

    Discord bot updates

--- The House Discord Bot/The House Discord Bot/TheHouseBot.cs
+++ The House Discord Bot/The House Discord Bot/TheHouseBot.cs
@@ -13,6 +13,7 @@ using System.Net.Http;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using The_House_Discord_Bot.DiscordFunctions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Tooling.Connector;
@@ -29,7 +30,10 @@ namespace The_House_Discord_Bot
         private CommandService _command;
         private IServiceProvider _servicePriveProvider;
         private IOrganizationService _crmConn;
-        private string botTrigger = "thb! ";
+        //Live Bot Trigger
+        //private string botTrigger = "thb! ";
+        //Test Bot Trigger
+        private string botTrigger = "thbt! ";
         private int recurrenceFlag = 0;
         private int barrensChatActivity = 0;
 
@@ -61,9 +65,14 @@ namespace The_House_Discord_Bot
             _client.MessageReceived += Client_MessageReceived;
             _client.Ready += Client_Ready;
             _client.Log += Client_Log;
+            _client.ReactionAdded += Client_ReactionAdded;
+            _client.ReactionRemoved += Client_ReactionRemoved;
 
             await _command.AddModulesAsync(Assembly.GetEntryAssembly(), _servicePriveProvider);
-            await _client.LoginAsync(TokenType.Bot, "NTg4NDgyNTAzOTcxNTY5Njkw.XQFxlQ.kOu5eynSGWL05-LJAL9XrbVAu8Y");
+            //Live Bot
+            //await _client.LoginAsync(TokenType.Bot, "NTg4NDgyNTAzOTcxNTY5Njkw.XQFxlQ.kOu5eynSGWL05-LJAL9XrbVAu8Y");
+            //Test Bot
+            await _client.LoginAsync(TokenType.Bot, "NjEwODgwNTYwMTA0OTk2ODg0.XVLtjQ.jkFf9GkyyOiffLpwXPtdtEkUKIQ");
             await _client.StartAsync();
             await Task.Delay(-1);
         }
@@ -117,6 +126,33 @@ namespace The_House_Discord_Bot
                 await Context.Channel.SendMessageAsync("Sorry but I did not recognize this command, please type **" + botTrigger + "-help** for a list of available commands that I understand.", false, null);
             }
         }
-        
+        private async Task Client_ReactionRemoved(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
+        {
+            //Grant roles to users that choose their class and role when reacting to the "Choose your role" post under member-information
+            if (arg1.Id == 610883091241631787)
+            {
+                IGuildUser user = (IGuildUser)arg3.User.Value;
+                IRole reactionRole = UserRoles.RoleInformation(arg3);
+                if (reactionRole != null)
+                {
+                    await (user).RemoveRoleAsync(reactionRole);
+                }
+            }
+        }
+
+        private async Task Client_ReactionAdded(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
+        {
+            //Grant roles to users that choose their class and role when reacting to the "Choose your role" post under member-information
+            if (arg1.Id == 610883091241631787)
+            {
+                IGuildUser user = (IGuildUser)arg3.User.Value;
+                IRole reactionRole = UserRoles.RoleInformation(arg3);
+                if (reactionRole != null)
+                {
+                    await (user).AddRoleAsync(reactionRole);
+                }
+            }
+            
+        }
     }
 }
